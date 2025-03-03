12e1d89f2af4384009b6fea97874163ab1d8febd
CamouflageNetwork <notmrmacattax@gmail.com>
2025-03-03 11:35:50 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,26 +1,22 @@
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 
-export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+const app = express();
 
-    if (req.method === "OPTIONS") {
-        return res.status(200).end();
-    }
+app.get('/proxy', async (req, res) => {
+  const targetUrl = req.query.url;
 
-    try {
-        const url = req.query.url;
-        if (!url) return res.status(400).send("No URL provided");
+  if (!targetUrl) {
+    return res.status(400).send('No URL provided');
+  }
 
-        const response = await axios.get(url, {
-            responseType: "arraybuffer",
-            headers: { "User-Agent": req.headers["user-agent"] }
-        });
+  try {
+    const response = await axios.get(targetUrl);
+    res.set('Content-Type', response.headers['content-type']);
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`);
+  }
+});
 
-        res.setHeader("Content-Type", response.headers["content-type"]);
-        res.send(response.data);
-    } catch (error) {
-        res.status(500).send(`Error: ${error.message}`);
-    }
-}
+module.exports = app;
