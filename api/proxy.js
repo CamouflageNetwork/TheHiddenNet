20989ba47eb1279c6dbe7f5745a331ced1a96bfb
CamouflageNetwork <notmrmacattax@gmail.com>
2025-03-03 11:40:35 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 
 const app = express();
 
+// This is where we handle proxying
 app.get('/proxy', async (req, res) => {
   const targetUrl = req.query.url;
-
+  
   if (!targetUrl) {
     return res.status(400).send('No URL provided');
   }
@@ -19,4 +20,5 @@ app.get('/proxy', async (req, res) => {
   }
 });
 
+// Export the Express app for Vercel to handle it as a serverless function
 module.exports = app;
