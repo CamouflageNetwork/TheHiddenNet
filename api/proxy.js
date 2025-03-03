9fffa710c6cbfd81012ec3c658948fd578cbba5f
CamouflageNetwork <notmrmacattax@gmail.com>
2025-03-03 11:50:10 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,24 +1,17 @@
-const express = require('express');
 const axios = require('axios');
 
-const app = express();
-
-// This is where we handle proxying
-app.get('/proxy', async (req, res) => {
-  const targetUrl = req.query.url;
+module.exports = async (req, res) => {
+  const { url } = req.query;
   
-  if (!targetUrl) {
-    return res.status(400).send('No URL provided');
+  if (!url) {
+    return res.status(400).send('Error: Missing "url" parameter');
   }
 
   try {
-    const response = await axios.get(targetUrl);
+    const response = await axios.get(url);
     res.set('Content-Type', response.headers['content-type']);
     res.status(200).send(response.data);
   } catch (error) {
-    res.status(500).send(`Error: ${error.message}`);
+    res.status(500).send(`Error fetching the URL: ${error.message}`);
   }
-});
-
-// Export the Express app for Vercel to handle it as a serverless function
-module.exports = app;
+};
